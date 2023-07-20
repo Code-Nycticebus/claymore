81282868faef06c6fd5ad2790ba0776c630c1928
Code-Nycticebus <loris.kriyonas@gmail.com>
2023-07-20 22:31:53 +0200

    styling

--- claymore/src/claymore/events/key.c
+++ claymore/src/claymore/events/key.c
@@ -3,11 +3,11 @@
 static CmKeyAction key_state[CM_KEY_COUNT] = {0};
 
 void cm_key_set(CmKeyCodes key, CmKeyAction action) {
-  assert(key > 0 && key < CM_KEY_COUNT);
+  assert(0 < key && key < CM_KEY_COUNT);
   key_state[key] = action;
 }
 
 CmKeyAction cm_key(CmKeyCodes key) {
-  assert(key > 0 && key < CM_KEY_COUNT);
+  assert(0 < key && key < CM_KEY_COUNT);
   return key_state[key];
 }
