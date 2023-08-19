3a0d2b84ad6c2c61c54b8c2264dab0e150d29f55
Code-Nycticebus <loris.kriyonas@gmail.com>
2023-08-19 15:44:31 +0200

    updated dependency build script. again...

--- deps/build_win32.bat
+++ deps/build_win32.bat
@@ -1,12 +1,15 @@
 @echo off
 cd cglm
+echo building clgm
 call build_win32.bat
 cd ..
 
 cd glew
+echo building glew
 python build_win32.py
 cd ..
 
 cd glfw
+echo building glfw
 python build_win32.py
 cd ..
--- deps/glfw/build_win32.py
+++ deps/glfw/build_win32.py
@@ -7,7 +7,6 @@ import os
 r = requests.get(
     "https://github.com/glfw/glfw/releases/download/3.3.8/glfw-3.3.8.bin.WIN32.zip"
 )
-print(r.status_code)
 with open("glfw.zip", "wb") as f:
     f.write(r.content)
 with zipfile.ZipFile("glfw.zip", "r") as zip:
