9855d33a519140d34eca864d471cc46d91c2e01a
Code-Nycticebus <loris.kriyonas@gmail.com>
2023-08-19 09:23:33 +0200

    Formatted binary download script

--- deps/glew/build_win32.py
+++ deps/glew/build_win32.py
@@ -1,11 +1,16 @@
-import requests
+import requests  # type: ignore
 import zipfile
-r = requests.get("https://github.com/nigels-com/glew/releases/download/glew-2.2.0/glew-2.2.0-win32.zip")
-print(r.status_code)
+import os
+
+# Download binaries
+
+r = requests.get(
+    "https://github.com/nigels-com/glew/releases/download/glew-2.2.0/glew-2.2.0-win32.zip"
+)
 with open("glew.zip", "wb") as f:
     f.write(r.content)
-with zipfile.ZipFile('glew.zip', 'r') as zip:
+with zipfile.ZipFile("glew.zip", "r") as zip:
     zip.extractall()
-import os
+
 os.rename("glew-2.2.0", "glew-win")
 os.remove("glew.zip")
--- deps/glfw/build_win32.py
+++ deps/glfw/build_win32.py
@@ -1,11 +1,17 @@
-import requests
+import requests  # type: ignore
 import zipfile
-r = requests.get("https://github.com/glfw/glfw/releases/download/3.3.8/glfw-3.3.8.bin.WIN32.zip")
+import os
+
+# Download binaries
+
+r = requests.get(
+    "https://github.com/glfw/glfw/releases/download/3.3.8/glfw-3.3.8.bin.WIN32.zip"
+)
 print(r.status_code)
 with open("glfw.zip", "wb") as f:
     f.write(r.content)
-with zipfile.ZipFile('glfw.zip', 'r') as zip:
+with zipfile.ZipFile("glfw.zip", "r") as zip:
     zip.extractall()
-import os
+
 os.rename("glfw-3.3.8.bin.WIN32", "glfw-win")
 os.remove("glfw.zip")
