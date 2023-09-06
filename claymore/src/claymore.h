7abfd273f7d3eb7eb29a97ae6b2e627c0e21c081
Code-Nycticebus <loris.kriyonas@gmail.com>
2023-09-06 09:05:56 +0200

    also put textures in their place

--- claymore/src/claymore.h
+++ claymore/src/claymore.h
@@ -2,14 +2,14 @@
 #define __CM_CLAYMORE_H__
 
 #include "claymore/core/app.h"
-#include "claymore/core/font.h"
 #include "claymore/core/scene.h"
-#include "claymore/core/textures.h"
 #include "claymore/core/window.h"
 #include "claymore/events/event.h"
 #include "claymore/logger/logger.h"
+#include "claymore/renderer/font.h"
 #include "claymore/renderer/renderer.h"
 #include "claymore/renderer/shader.h"
+#include "claymore/renderer/textures.h"
 #include "claymore/sound/sound.h"
 
 // TODO: Should not be public
