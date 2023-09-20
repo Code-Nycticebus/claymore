385bc2c8e9e006aec2bb19f3a3d6a46982189dd2
Code-Nycticebus <loris.kriyonas@gmail.com>
2023-09-20 07:15:55 +0200

    made it compile on any platform

--- sandbox/src/framebuffer/framebuffer_layer.c
+++ sandbox/src/framebuffer/framebuffer_layer.c
@@ -19,7 +19,7 @@ static CmRenderBuffer rb2;
 
 static bool effect = true;
 
-static char *_strdup(const char *str) {
+static char *custom_strdup(const char *str) {
   size_t size = strlen(str) + 1;
   char *s = calloc(size, sizeof(char));
   strcpy(s, str);
@@ -47,7 +47,7 @@ void drop_callback(GLFWwindow *window, int count, const char **paths) {
   (void)window;
   for (int i = 0; i < count; i++) {
     free(fs_file);
-    fs_file = _strdup(paths[i]);
+    fs_file = custom_strdup(paths[i]);
     reload_shader();
   }
 }
@@ -89,7 +89,7 @@ static bool framebuffer_init(CmScene *scene, CmLayer *layer) {
   framebuffer_shader = cm_shader_load_from_file(
       "res/shader/framebuffer.vs.glsl", "res/shader/framebuffer.fs.glsl");
 
-  fs_file = _strdup("res/shader/effect.fs.glsl");
+  fs_file = custom_strdup("res/shader/effect.fs.glsl");
   effect_shader = cm_shader_load_from_file(vs_file, fs_file);
 
   framebuffer = cm_framebuffer_create(scene->app->window->width,
