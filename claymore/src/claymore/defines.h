9cd930c81f3e3758b84fe860dc3ffe429374a92b
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-02-14 22:27:31 +0100

    added vec formatting

--- claymore/src/claymore/defines.h
+++ claymore/src/claymore/defines.h
@@ -4,6 +4,15 @@
 #include "cglm/cglm.h" // IWYU pragma: export
 #include "clib/clib.h" // IWYU pragma: export
 
+#define VEC2_FMT "%f, %f"
+#define VEC2_ARG(v) v[0], v[1]
+
+#define VEC3_FMT "%f, %f, %f"
+#define VEC3_ARG(v) v[0], v[1], v[2]
+
+#define VEC4_FMT "%f, %f, %f, %f"
+#define VEC4_ARG(v) v[0], v[1], v[2], v[3]
+
 #ifndef NDEBUG
 #define _CM_DEBUG
 #endif
