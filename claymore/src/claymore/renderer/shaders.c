25059426b2abee17da6f33571a3c3663dbd1cca2
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-02-14 23:50:42 +0100

    Ported shaders to clib

---
+++ claymore/src/claymore/renderer/shaders.c
@@ -0,0 +1,166 @@
+#include "shaders.h"
+
+#include "GL/glew.h"
+#include "clib/core/error.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define _cm_check_shader(error, shader_id, gl_check, gl_get_iv, gl_get_log)    \
+  {                                                                            \
+    GLint result = GL_FALSE;                                                   \
+    GLsizei lenght = 0;                                                        \
+    gl_get_iv(shader_id, gl_check, &result);                                   \
+    gl_get_iv(shader_id, GL_INFO_LOG_LENGTH, &lenght);                         \
+    if (lenght > 0) {                                                          \
+      char *err_msg = calloc(sizeof(char), lenght);                            \
+      gl_get_log(shader_id, lenght, NULL, err_msg);                            \
+      error_emit(error, -1, "%s", err_msg);                                    \
+      free(err_msg);                                                           \
+      return false;                                                            \
+    }                                                                          \
+    return true;                                                               \
+  }
+
+static bool _cm_shader_check_error(GLuint shader_id, GLenum gl_check,
+                                   Error *error) {
+  if (gl_check == GL_COMPILE_STATUS) {
+    _cm_check_shader(error, shader_id, gl_check, glGetShaderiv,
+                     glGetShaderInfoLog);
+  }
+  if (gl_check == GL_LINK_STATUS) {
+    _cm_check_shader(error, shader_id, gl_check, glGetProgramiv,
+                     glGetProgramInfoLog);
+  }
+
+  clib_assert(gl_check == GL_COMPILE_STATUS || gl_check == GL_LINK_STATUS,
+              "Other checks are not supported");
+  return false;
+}
+
+#undef _cm_check_shader
+
+static GLuint _cm_compile_shader(Str shader_src, GLenum type, Error *error) {
+  clib_assert((type == GL_VERTEX_SHADER || type == GL_FRAGMENT_SHADER),
+              "Shader only supports these types for now!");
+
+  GLuint shader_id = glCreateShader(type);
+
+  const GLint len = shader_src.len;
+  glShaderSource(shader_id, 1, &shader_src.data, &len);
+  glCompileShader(shader_id);
+
+  _cm_shader_check_error(shader_id, GL_COMPILE_STATUS, error);
+  error_propagate(error, { error_add_note(STR_FMT, STR_ARG(shader_src)); });
+
+  return shader_id;
+}
+
+CmShader cm_shader_load_from_file(Str vs_path, Str fs_path, Error *error) {
+  CmShader program = {0};
+  Arena temp = {0};
+
+  Str vs_content = file_read_str(vs_path, &temp, error);
+  error_propagate(error, { goto defer; });
+
+  Str fs_content = file_read_str(fs_path, &temp, error);
+  error_propagate(error, { goto defer; });
+
+  program = cm_shader_load_from_memory(vs_content, fs_content, error);
+  error_propagate(error, { goto defer; });
+
+defer:
+  arena_free(&temp);
+  return program;
+}
+
+CmShader cm_shader_load_from_memory(Str vs, Str fs, Error *error) {
+  CmShader program = {0};
+  GLuint vs_id = 0;
+  GLuint fs_id = 0;
+
+  vs_id = _cm_compile_shader(vs, GL_VERTEX_SHADER, error);
+  error_propagate(error, { goto defer; });
+
+  fs_id = _cm_compile_shader(fs, GL_FRAGMENT_SHADER, error);
+  error_propagate(error, { goto defer; });
+
+  program.id = glCreateProgram();
+
+  glAttachShader(program.id, vs_id);
+  glAttachShader(program.id, fs_id);
+  glLinkProgram(program.id);
+
+  _cm_shader_check_error(program.id, GL_LINK_STATUS, error);
+  error_propagate(error, { goto defer; });
+
+defer:
+  if (vs_id) {
+    glDeleteShader(vs_id);
+  }
+  if (fs_id) {
+    glDeleteShader(fs_id);
+  }
+  return program;
+}
+
+void cm_shader_delete(CmShader *shader) { glDeleteProgram(shader->id); }
+
+void cm_shader_bind(const CmShader *shader) { glUseProgram(shader->id); }
+void cm_shader_unbind(void) { glUseProgram(0); }
+
+GLint cm_shader_get_uniform_location(CmShader *shader, Str uniform_name) {
+  u64 hash = str_hash(uniform_name);
+  glUseProgram(shader->id);
+  for (size_t i = 0; i < shader->uniform_count; i++) {
+    if (shader->uniforms[i].hash == hash) {
+      return shader->uniforms[i].location;
+    }
+  }
+
+  Arena temp = {0};
+
+  Str name = str_copy(uniform_name, &temp);
+  GLint location = glGetUniformLocation(shader->id, name.data);
+  if (location == -1) {
+    clib_log_error("Uniform location '" STR_FMT "' not found in shader %u\n",
+                   STR_ARG(uniform_name), shader->id);
+    return -1;
+  }
+  arena_free(&temp);
+
+  clib_assert(shader->uniform_count < CM_SHADER_UNIFORM_MAX,
+              "shader has to many cached uniforms");
+
+  shader->uniforms[shader->uniform_count].location = location;
+  shader->uniforms[shader->uniform_count].hash = hash;
+  shader->uniform_count++;
+
+  return location;
+}
+
+void cm_shader_set_mat4(CmShader *shader, Str uniform_name, const mat4 mat) {
+  GLint location = cm_shader_get_uniform_location(shader, uniform_name);
+  glUniformMatrix4fv(location, 1, GL_FALSE, (float *)&mat);
+}
+
+void cm_shader_set_vec3(CmShader *shader, Str uniform_name, const vec3 vec) {
+  GLint location = cm_shader_get_uniform_location(shader, uniform_name);
+  glUniform3fv(location, 1, (float *)&vec);
+}
+
+void cm_shader_set_vec4(CmShader *shader, Str uniform_name, const vec4 vec) {
+  GLint location = cm_shader_get_uniform_location(shader, uniform_name);
+  glUniform4fv(location, 1, (float *)&vec);
+}
+
+void cm_shader_set_f32(CmShader *shader, Str uniform_name, float value) {
+  GLint location = cm_shader_get_uniform_location(shader, uniform_name);
+  glUniform1f(location, value);
+}
+
+void cm_shader_set_i32(CmShader *shader, Str uniform_name, int32_t value) {
+  GLint location = cm_shader_get_uniform_location(shader, uniform_name);
+  glUniform1i(location, value);
+}
---
+++ claymore/src/claymore/renderer/shaders.h
@@ -0,0 +1,33 @@
+#ifndef __CLAYMORE_SHADERS_H__
+#define __CLAYMORE_SHADERS_H__
+
+#include "claymore/defines.h" // IWYU pragma: export
+
+#ifndef CM_SHADER_UNIFORM_MAX
+#define CM_SHADER_UNIFORM_MAX 10
+#endif
+
+typedef struct {
+  i32 id;
+  usize uniform_count;
+  struct {
+    u64 hash;
+    i32 location;
+  } uniforms[CM_SHADER_UNIFORM_MAX];
+} CmShader;
+
+CmShader cm_shader_load_from_file(Str vs_path, Str fs_path, Error *error);
+CmShader cm_shader_load_from_memory(Str vs, Str fs, Error *error);
+
+void cm_shader_delete(CmShader *shader);
+
+void cm_shader_bind(const CmShader *shader);
+void cm_shader_unbind(void);
+i32 cm_shader_get_uniform_location(CmShader *shader, Str uniform_name);
+void cm_shader_set_mat4(CmShader *shader, Str uniform_name, const mat4 mat);
+void cm_shader_set_vec3(CmShader *shader, Str uniform_name, const vec3 vec);
+void cm_shader_set_vec4(CmShader *shader, Str uniform_name, const vec4 vec);
+void cm_shader_set_f32(CmShader *shader, Str uniform_name, float value);
+void cm_shader_set_i32(CmShader *shader, Str uniform_name, int32_t value);
+
+#endif // !__CLAYMORE_SHADERS_H__
