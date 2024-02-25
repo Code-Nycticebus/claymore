36a69696f923d4e9e5fcaf2896baf10481067c9d
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-02-25 23:01:31 +0100

    Cleaned up sandbox

--- sandbox/src/sandbox.c
+++ sandbox/src/sandbox.c
@@ -24,35 +24,22 @@ static void event(CmScene *scene, CmEvent *event) {
   Sandbox *sandbox = scene->data;
   (void)sandbox, (void)event;
   cm_event_key(event, {
-    if (key->action == CM_KEY_PRESS && key->code == CM_KEY_0) {
-      cm_scene_push(scene, fps_scene_init);
+    if (key->action == CM_KEY_PRESS && key->code == CM_KEY_ESCAPE) {
+      cm_window_close(true);
     }
   });
 }
 
 static void init(CmScene *scene) {
-  clib_log_info("sandbox init");
   Sandbox *sandbox = arena_alloc(&scene->arena, sizeof(Sandbox));
   scene->data = sandbox;
   sandbox->count = 0;
   cm_scene_push(scene, fps_scene_init);
 }
 
-static void update(CmScene *scene, double deltatime) {
-  Sandbox *sandbox = scene->data;
-  (void)sandbox, (void)deltatime;
-}
-
-static void final(CmScene *scene) {
-  (void)scene;
-  clib_log_info("sandbox final");
-}
-
 static CmSceneInterface *scene_init(void) {
   static CmSceneInterface sandbox = {
       .init = init,
-      .update = update,
-      .final = final,
       .event = event,
   };
   return &sandbox;
--- sandbox/src/scenes/fps.c
+++ sandbox/src/scenes/fps.c
@@ -2,24 +2,18 @@
 
 typedef struct {
   float timer;
-  usize id;
 } Fps;
 
 static void init(CmScene *scene) {
   Fps *fps = arena_calloc(&scene->arena, sizeof(Fps));
   scene->data = fps;
   fps->timer = 0;
-
-  usize *count = cm_scene_parent(scene)->data;
-  clib_log_info("Count: %" USIZE_FMT, *count);
-  fps->id = *count;
-  (*count)++;
 }
 
 static void update(CmScene *scene, double deltatime) {
   Fps *fps = scene->data;
   if ((fps->timer += deltatime) >= 1) {
-    clib_log_info("%" USIZE_FMT ": %.0f fps", fps->id, 1 / deltatime);
+    clib_log_info("%.0f fps", 1 / deltatime);
     fps->timer = 0;
   }
 }
