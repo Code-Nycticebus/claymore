fc0a9b8df7e09425a2a9ee165d13e757394b630b
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-05-22 22:44:32 +0200

    did the youtube tutorial

--- sandbox/assets/shader/toy.fs.glsl
+++ sandbox/assets/shader/toy.fs.glsl
@@ -1,13 +1,34 @@
 
 
+vec3 palette(float t) {
+  vec3 a = vec3(0.8, 0.2, 0.5);
+  vec3 b = vec3(0.5, 0.9, 0.3);
+  vec3 c = vec3(0.1, 0.23, 1.0);
+  vec3 d = vec3(0.432, 0.213, 0.642);
+
+  return a + b * cos(6.28318 * (c * t + d));
+}
+
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
   vec2 uv = fragCoord.xy / u_resolution * 2 - 1;
-  uv.x *= u_resolution.x / u_resolution.y;
+  vec2 uv0 = uv;
+  // vec2 uv0 = uv * u_resolution.x / u_resolution.y;
+  vec3 finalColor = vec3(0);
 
-  float d = length(uv);
-  
-  d -= 0.5;
-  d = abs(d);
+  for (float i = 0.0; i < 2.0; i++) {
+    uv = fract(uv * 1.8) - 0.5;
+    float d = length(uv) * exp(-length(uv0));
 
-  fragColor = vec4(d, d, d, 1);
+    vec3 col = palette(length(uv0) + i*.8 + u_time*.4);
+    
+    const float freq = 8.;
+    d = cos(d*freq+u_time*.4)/freq;
+    d = abs(d);
+
+    d = pow(0.01 / d, 1.2);
+
+    finalColor += col *= d;
+  }
+
+  fragColor = vec4(finalColor, 1);
 }
