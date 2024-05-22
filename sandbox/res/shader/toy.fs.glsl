0d98b72495242ebf74088bc647220403d477acac
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-05-22 07:05:28 +0200

    implemented https://www.shadertoy.com/view/mtyGWy for a test

--- sandbox/res/shader/toy.fs.glsl
+++ sandbox/res/shader/toy.fs.glsl
@@ -9,7 +9,44 @@ uniform vec2 u_resolution;
 uniform float u_deltatime;
 uniform float u_fps;
 
+
+/* This animation is the material of my first youtube tutorial about creative 
+   coding, which is a video in which I try to introduce programmers to GLSL 
+   and to the wonderful world of shaders, while also trying to share my recent 
+   passion for this community.
+                                       Video URL: https://youtu.be/f4s1h2YETNY
+*/
+
+//https://iquilezles.org/articles/palettes/
+vec3 palette( float t ) {
+    vec3 a = vec3(0.5, 0.5, 0.5);
+    vec3 b = vec3(0.5, 0.5, 0.5);
+    vec3 c = vec3(1.0, 1.0, 1.0);
+    vec3 d = vec3(0.263,0.416,0.557);
+
+    return a + b*cos( 6.28318*(c*t+d) );
+}
+
+//https://www.shadertoy.com/view/mtyGWy
 void main() {
-	vec2 st = gl_FragCoord.xy/u_resolution;
-	frag_color = vec4(st.x,st.y,0.0,1.0);
+    vec2 uv = (gl_FragCoord.xy * 2.0 - u_resolution.xy) / u_resolution.y;
+    vec2 uv0 = uv;
+    vec3 finalColor = vec3(0.0);
+    
+    for (float i = 0.0; i < 4.0; i++) {
+        uv = fract(uv * 1.5) - 0.5;
+
+        float d = length(uv) * exp(-length(uv0));
+
+        vec3 col = palette(length(uv0) + i*.4 + u_time*.4);
+
+        d = sin(d*8. + u_time)/8.;
+        d = abs(d);
+
+        d = pow(0.01 / d, 1.2);
+
+        finalColor += col * d;
+    }
+        
+    frag_color = vec4(finalColor, 1.0);
 }
