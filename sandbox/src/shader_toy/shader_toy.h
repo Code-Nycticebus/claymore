c5d05890133d367eb30ff450f413fa56a8290fbc
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-05-24 09:29:52 +0200

    moved shadertoy to own repo

--- sandbox/src/shader_toy/shader_toy.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SANDBOX_SHADER_H__
-#define __SANDBOX_SHADER_H__
-
-#include "claymore.h"
-
-CmScene *shader_init(CmScene *parent, Str filename, Error *error);
-CmScene *error_display_init(CmScene *parent, Str message);
-
-#endif /* !__SANDBOX_SHADER_H__ */
