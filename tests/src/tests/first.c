99a52c079978110769b8bc67cb8386d4e3afb997
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-08-02 22:30:34 +0200

    added stb_image_write to libs

--- tests/src/tests/first.c
+++ tests/src/tests/first.c
@@ -1,8 +1,19 @@
 #include "tests.h"
 
+#include "stb_image_write.h"
+
 static void init(CmScene *scene) {
   (void)scene;
   cebus_log("YES");
+
+  stbi_write_png("test.png", 2, 2, 3,
+                 (u8[]){
+                     255, 255, 0, //
+                     255, 0, 255, //
+                     255, 0, 255, //
+                     255, 255, 0, //
+                 },
+                 2 * 3);
 }
 static void frame_update(CmScene *scene) { (void)scene; }
 
