2da4037116375d60cde71e7ffdd72a421b63c5da
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-08-04 12:55:56 +0200

    added different run targets

--- tests/src/display.c
+++ tests/src/display.c
@@ -1,9 +1,11 @@
-#include "tests/tests.h"
 #include "claymore/entrypoint.h"
+#include "tests/tests.h"
 
-#include "suite/suite.h"
+#include "record/display.h"
 
-static void init(CmScene *scene) { suite(scene, WIDHT, HEIGHT, ARRAY_LEN(test_suite), test_suite); }
+static void init(CmScene *scene) {
+  display(scene, WIDHT, HEIGHT, ARRAY_LEN(test_suite), test_suite);
+}
 
 static CmSceneInterface *root(void) {
   static CmSceneInterface interface = {
@@ -14,7 +16,7 @@ static CmSceneInterface *root(void) {
 
 ClaymoreConfig *claymore_init(void) {
   static ClaymoreConfig config = {
-      .window = {.width = WIDHT, .height = HEIGHT, .title = "TESTS"},
+      .window = {.width = WIDHT, .height = HEIGHT, .title = "DISPLAY"},
       .root = root,
   };
   return &config;
