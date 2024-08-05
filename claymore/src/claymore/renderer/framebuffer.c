26ebdfdf9ffc8bebdda5729ea653843c6519443f
Code-Nycticebus <loris.kriyonas@gmail.com>
2024-08-05 05:09:44 +0200

    formatting framebuffer

--- claymore/src/claymore/renderer/framebuffer.c
+++ claymore/src/claymore/renderer/framebuffer.c
@@ -15,11 +15,11 @@ CmFramebuffer cm_framebuffer_create(CmGpu *gpu, usize width, usize heigth) {
 CmTexture cm_framebuffer_attach_texture_color(CmFramebuffer *fb) {
   cm_gpu_fbo_bind(fb->fbo);
 
-  CmTexture texture = cm_texture_from_bytes(
-      fb->gpu, NULL, (CmTextureFormat){.bpp = 4, .w = fb->size[0], .h = fb->size[1]});
+  const CmTextureFormat format = {.bpp = 4, .w = fb->size[0], .h = fb->size[1]};
+  CmTexture texture = cm_texture_from_bytes(fb->gpu, NULL, format);
 
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + fb->attachment_count++,
-                         GL_TEXTURE_2D, texture.id, 0);
+  const u32 attachment = GL_COLOR_ATTACHMENT0 + fb->attachment_count++;
+  glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, GL_TEXTURE_2D, texture.id, 0);
   cm_gpu_fbo_bind(0);
 
   fb->mask |= GL_COLOR_BUFFER_BIT;
